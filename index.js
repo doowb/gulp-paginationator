b2fd71df9c21aeb4cb3e7e84539e061bfa13ceb3
Brian Woodward <brian.woodward@gmail.com>
2016-08-09 19:30:28 -0400

    add options for specifying `prop`, `filter`, and `sort`

--- index.js
+++ index.js
@@ -2,28 +2,68 @@
 
 var utils = require('./lib/utils');
 
-module.exports = function(options) {
-  var opts = utils.extend({}, options);
+
+/**
+ * Add pagination data to each file coming through the stream.
+ *
+ * ```js
+ * gulp.task('default', function() {
+ *   return gulp.src(['*.hbs'])
+ *     .pipe(paginationator())
+ *     .on('data', function(file) {
+ *       console.log(file);
+ *       console.log(file.data.pager);
+ *     });
+ * });
+ * ```
+ * @param  {Object} `options` Options to control which files are paginated and where the data is put.
+ * @param  {String} `options.prop` This is the property that the pagination data will be set on. Defaults to `pager`.
+ * @param  {Function} `options.filter` Function that may be used to control which files are paginated.
+ * @param  {Function} `options.sort` Function that may be used to sort the files before paginating.
+ * @return {Stream} Stream to be passed to `.pipe` that will add pagination data to each file.
+ * @api public
+ */
+
+module.exports = function paginationator(options) {
+  var opts = utils.extend({prop: 'pager'}, options);
 
   var files = [];
   return utils.through.obj(function(file, enc, cb) {
     files.push(file);
     cb(null);
   }, function(cb) {
+    var removed = [];
     var stream = this;
     var Page = utils.paginationator.Page;
     var Pages = utils.paginationator.Pages;
     var pages = new Pages();
 
+    if (typeof opts.filter === 'function') {
+      files = files.filter(function(file) {
+        var keep = opts.filter.apply(opts.filter, arguments);
+        if (!keep) removed.push(file);
+        return keep;
+      });
+    }
+
+    if (typeof opts.sort === 'function') {
+      files.sort(opts.sort);
+    }
+
     // need to loop over files to apply all pagination objects so the correct `next/prev` values are accessible
     files.forEach(function(file) {
       file.data = file.data || {};
-      file.data.pager = new Page();
-      pages.addPage(file.data.pager);
+      file.data[opts.prop] = new Page();
+      pages.addPage(file.data[opts.prop]);
     });
 
     // this loop is for pushing files back into the stream
     files.forEach(function(file) {
+      stream.push(file);
+    });
+
+    // push any files removed through a filter back into the stream
+    removed.forEach(function(file) {
       stream.push(file);
     });
 
