d342b5510f37d2221b6b1addaac042c1745a5b07
Brian Woodward <brian.woodward@gmail.com>
2016-08-09 18:08:35 -0400

    implementation

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 'use strict';
 
-var through = require('through2');
+var utils = require('./lib/utils');
 
 module.exports = function(options) {
-  return through.obj(function(file, enc, next) {
-    if (file.isNull()) {
-      next(null, file);
-      return;
-    }
+  var opts = utils.extend({}, options);
 
-    var str = file.contents.toString();
-    file.contents = new Buffer(str);
-    next(null, file);
+  var files = [];
+  return utils.through.obj(function(file, enc, cb) {
+    files.push(file);
+    cb(null);
+  }, function(cb) {
+    var stream = this;
+    var Page = utils.paginationator.Page;
+    var Pages = utils.paginationator.Pages;
+    var pages = new Pages();
+
+    // need to loop over files to apply all pagination objects so the correct `next/prev` values are accessible
+    files.forEach(function(file) {
+      file.data = file.data || {};
+      file.data.pager = new Page();
+      pages.addPage(file.data.pager);
+    });
+
+    // this loop is for pushing files back into the stream
+    files.forEach(function(file) {
+      stream.push(file);
+    });
+
+    cb();
   });
 };
